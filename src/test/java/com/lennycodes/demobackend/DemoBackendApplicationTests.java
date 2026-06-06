1b244f32f01d3aea3477b6368d04947bd37e679b
Leibniz H. Berihuete Nunez <leibniz.berihuete@gmail.com>
2026-06-05 20:54:35 -0400

    fixes unit tests

--- src/test/java/com/lennycodes/demobackend/DemoBackendApplicationTests.java
+++ src/test/java/com/lennycodes/demobackend/DemoBackendApplicationTests.java
@@ -1,13 +1,46 @@
 package com.lennycodes.demobackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DemoBackendApplicationTests {
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
   @Test
   void contextLoads() {
+    assertNotNull(applicationContext, "Application context should not be null");
+  }
+
+  @Test
+  void applicationContextContainsMainBean() {
+    assertTrue(
+        applicationContext.containsBean("demoBackendApplication"),
+        "Application context should contain the main application bean"
+    );
+  }
+
+  @Test
+  void mainBeanIsCorrectType() {
+    Object bean = applicationContext.getBean("demoBackendApplication");
+    assertInstanceOf(DemoBackendApplication.class, bean);
+  }
+
+  @Test
+  void mainMethodRunsWithoutException() {
+    assertDoesNotThrow(() -> DemoBackendApplication.main(new String[]{}));
+  }
+
+  @Test
+  void applicationContextHasExpectedEnvironment() {
+    assertNotNull(applicationContext.getEnvironment(), "Environment should not be null");
+    assertNotNull(applicationContext.getId(), "Application context ID should not be null");
   }
 
 }
